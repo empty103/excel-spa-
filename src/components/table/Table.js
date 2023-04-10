6120c5ba067142c3008632d755ab6a331e6c5d6d
Yan103 <fanart48406@gmail.com>
2023-04-10 20:48:05 +0300

    feat: добавлена ячейка по умолчанию, выделение ячейки по клику и множественное выделенние ячеек по шифт+клик

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,8 +1,11 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
 import { createTable } from "./table.template";
+
 import { $ } from '../../core/dom';
 import { resizeHandler } from "./table.resize";
-import { shouldResize } from "./table.function";
+
+import { isCell, matrix, shouldResize } from "./table.function";
+import { TableSelection } from "./TableSelection";
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -17,9 +20,30 @@ export class Table extends ExcelComponent {
     return createTable(30);
   }
 
+  prepare() {
+    this.selection = new TableSelection();
+  }
+
+  init() {
+    super.init();
+    const $cell = this.$root.find(`[data-id="0:0"]`);
+    this.selection.select($cell);
+  }
+
+
   onMousedown(event) {
     if (shouldResize(event)) {
       return resizeHandler(this.$root, event);
+    } else if (isCell(event)) {
+      const $target = $(event.target)
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.current)
+          .map(id => this.$root.find(`[data-id="${id}"]`));
+        this.selection.selectGroup($cells);
+      } else {
+        this.selection.select($target);
+      }
     }
   }
 }
+
---
+++ src/components/table/TableSelection.js
@@ -0,0 +1,28 @@
+export class TableSelection {
+    static className = 'selected';
+
+    constructor() {
+        this.group = [];
+        this.current = null;
+    }
+
+    select($el) {
+        this.clear();
+
+        this.group.push($el);
+        $el.addClass(TableSelection.className);
+        this.current = $el;
+    }
+
+    selectGroup($group = []) {
+        this.clear();
+
+        this.group = $group;
+        this.group.forEach($el => $el.addClass(TableSelection.className));
+    }
+
+    clear() {
+        this.group.forEach($cell => $cell.removeClass('selected'));
+        this.group = [];
+    }
+}
