bf6bc9308b4f541aabe64d96eb18f93564a4f7f3
empty103 <101938324+empty103@users.noreply.github.com>
2023-04-30 21:18:50 +0500

    Routing (#6)
    * feat: создан роутер, рефакторинг страницы и переключение страниц
    
    * feat: работают кнопки в header (удаление и выход из страницы), исправлен баг с содержимым страницы: при переключении и создании страниц оставался контент прошлой
    
    * finish: добавлены даты открытия экселей

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,11 +1,10 @@
 import { Emitter } from "../../core/Emitter";
 import { StoreSubscriber } from "../../core/StoreSubscriber";
 import { $ } from "../../core/dom";
+import { updateDate } from "../../redux/actions";
 
 export class Excel {
-  constructor(selector, options) {
-    this.$el = $(selector);
-
+  constructor(options) {
     this.components = options.components || [];
     this.store = options.store;
 
@@ -33,8 +32,8 @@ export class Excel {
     return $root;
   }
 
-  render() {
-    this.$el.append(this.getRoot());
+  init() {
+    this.store.dispatch(updateDate);
     this.subscriber.subscribeComponents(this.components);
     this.components.forEach(component => component.init());
   }
---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,29 @@
+import { $ } from "../core/dom";
+import { Page } from "../core/routes/Page";
+import { createRecordsTable } from "./dashboard.function";
+
+export class DashboardPage extends Page {
+    getRoot() {
+        const now = Date.now().toString();
+
+        return $.create('div', 'db').html(
+            `
+                <div class="db__header">
+                    <h1>Панель управления</h1>
+                </div>
+                
+                <div class="db__new">
+                    <div class="db__view">
+                        <a href="#excel/${now}" class="db__create">
+                            Новая <br /> Таблица
+                        </a>
+                    </div>
+                </div>
+                
+                <div class="db__table db__view">
+                    ${createRecordsTable()}
+                </div>
+            `
+        );
+    }
+}
